7d07aa7bd8074e9c77dc024163a0eb14f9797d43
RRRuby1225 <wyr2115@gmail.com>
2025-09-15 12:54:06 +0800

    prepared for the development

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,19 +7,32 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 
+import path from 'path';
+
 const app = express();
 const PORT = process.env.PORT || 5001;
-
+const __dirname = path.resolve();
 
 
 // Middleware
-app.use(express.json());
-app.use(cors({
-    origin: 'http://localhost:5173',
-}));
-app.use(rateLimiter);
-app.use('/api/notes', notesRoutes);
+if (process.env.NODE_ENV === 'production') {
+    app.use(cors({
+        origin: 'http://localhost:5173',
+    }));
+}
 
+app.use(express.json());
+app.use(rateLimiter);
+app.use("/api/notes", notesRoutes);
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend", "dist", "index.html'));
+    });
+}
 
 
 connectDB().then(() => {
